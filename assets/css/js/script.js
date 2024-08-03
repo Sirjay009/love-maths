bee624c62f87b895a0940c8e13cd2d4778a687cb
Sirjay009 Obutte <obuttechima@rocketmail.com>
2024-08-03 00:35:32 +0000

    Add code toincrement score and incorrect answers tally

--- assets/css/js/script.js
+++ assets/css/js/script.js
@@ -47,8 +47,10 @@ function checkAnswer() {
 
     if(isCorrect) {
         alert ("Hey! You got it right! :D");
+        incrementScore();
     } else {
         alert (`Awww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -71,11 +73,23 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Get the current score from the DOM and increment it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
     
 }
 
+/**
+ * Get the current tally of incorrect answer from the DOM and increment it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
     
 }
 
